2175dc056d74e0afa099652f468e0b269aa5c127
Craig <craigmiller160@gmail.com>
2021-05-15 14:44:18 -0400

    Working on test setup

--- test/AjaxErrorHandler.test.ts
+++ test/AjaxErrorHandler.test.ts
@@ -1,3 +1,40 @@
+import createAjaxErrorHandler, { AjaxErrorHandlerConfig } from '../src';
+import { AxiosError } from 'axios';
+
+const responseMessageExtractor = jest.fn();
+const errorMessageHandler = jest.fn();
+const unauthorizedHandler = jest.fn();
+
+const config: AjaxErrorHandlerConfig = {
+	responseMessageExtractor,
+	errorMessageHandler,
+	unauthorizedHandler
+};
+
+const ajaxErrorHandler = createAjaxErrorHandler(config);
+
+const axiosError: AxiosError = {
+	message: 'Error message',
+	isAxiosError: true,
+	config: {},
+	name: 'Axios Error',
+	toJSON: () => ({}),
+	response: {
+		status: 500,
+		statusText: 'Internal Server Error',
+		headers: {},
+		config: {},
+		data: {
+			message: 'Hello World'
+		}
+	}
+};
+
+const error: Error = {
+	message: 'Error message',
+	name: 'Error'
+};
+
 describe('Ajax Error Handler', () => {
 	it('handles axios error', () => {
 		throw new Error();
